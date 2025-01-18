516438e680882e848be262ec040ef5b92ae7ad73
ibrahimghellab <ghellabibrahim@gmail.com>
2025-01-18 18:26:29 +0100

    fix: remove commented descriptions from SQL views for cleaner code

--- DB_Part/view.sql
+++ DB_Part/view.sql
@@ -1,4 +1,3 @@
---Liste les membres, leurs rôles, et les groupes auxquels ils appartiennent.
 
 CREATE VIEW VueMembresGroupes AS
 SELECT M.idMembre, I.nom, I.prenom, G.nomGroupe, R.nomRole, M.dateAdhesion
@@ -8,7 +7,6 @@ INNER JOIN Groupe G ON M.idGroupe = G.idGroupe
 INNER JOIN Role R ON M.idRole = R.idRole;
 
 
---Liste les propositions avec leur auteur, le statut actuel, et le vote associé.
 
 CREATE VIEW VuePropositions AS
 SELECT P.idProposition, P.titre, P.description, P.theme, P.status, P.dateCreation, V.typeScrutin, V.dateDebut, V.dateFin
@@ -16,7 +14,6 @@ FROM Proposition P
 LEFT JOIN Vote V ON P.idVote = V.idVote;
 
 
---Fournit les détails des commentaires signalés et les membres qui les ont signalés. #ajouter qui a écrit le commentaire
 
 CREATE VIEW VueCommentairesSignales AS
 SELECT C.idCommentaire, C.texte, C.dateCommentaire, C.status,C.idMembre as commentateur ,M.idMembre as signaleur, I.nom, I.prenom 
@@ -26,7 +23,6 @@ INNER JOIN Commentaire C ON S.idCommentaire = C.idCommentaire
 INNER JOIN Internaute I ON M.idInternaute = I.idInternaute;
 
 
---Donne une vue consolidée des votes avec les choix des membres.
 
 CREATE VIEW VueVotesChoix AS
 SELECT V.idVote, V.typeScrutin, V.dateDebut, V.dateFin, MV.idMembre, I.nom AS NomVotant, I.prenom AS PrenomVotant, MV.choix
@@ -36,7 +32,6 @@ INNER JOIN Membre M ON MV.idMembre = M.idMembre
 INNER JOIN Internaute I ON M.idInternaute = I.idInternaute;
 
 
---Affiche toutes les notifications reçues par les internautes, avec leurs détails.
 
 CREATE VIEW VueNotificationsReçues AS
 SELECT INN.idInternaute, I.nom, I.prenom, N.message, N.typeNotification, N.dateNotification, N.status AS StatutNotification
