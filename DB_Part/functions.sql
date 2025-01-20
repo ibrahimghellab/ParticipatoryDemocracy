4ed592973e5d98c09af08ee9dc24db6ca579d7ec
ibrahimghellab <ghellabibrahim@gmail.com>
2025-01-21 00:11:33 +0100

    Refactor createGroupe procedure to include member insertion and rename createInternaute procedure for clarity

--- DB_Part/functions.sql
+++ DB_Part/functions.sql
@@ -46,32 +46,6 @@ END;
 DELIMITER ;
 
 
-
---Crée un groupe 
-
-DELIMITER //
-
-CREATE OR REPLACE PROCEDURE creer_groupe(
-    IN p_nomGroupe VARCHAR(50),
-    IN p_imageGroupe VARCHAR(255),
-    IN p_couleurGroupe VARCHAR(50),
-    IN p_description VARCHAR(255)
-)
-BEGIN
-    DECLARE v_idGroupe INT;
-
-    INSERT INTO Groupe (nomGroupe, imageGroupe, couleurGroupe, dateCreation, description)
-    VALUES (p_nomGroupe, p_imageGroupe, p_couleurGroupe, NOW(), p_description);
-    
-    SET v_idGroupe = LAST_INSERT_ID();
-    
-    SELECT v_idGroupe;
-END;
-//
-
-DELIMITER ;
-
-
 DELIMITER //
 
 CREATE OR REPLACE PROCEDURE createGroupe(id INT ,nom VARCHAR(50),img VARCHAR(50),clr VARCHAR(50),descr VARCHAR(50)) 
@@ -82,6 +56,16 @@ SET crt_date=CURRENT_DATE;
 INSERT INTO Groupe (nomGroupe,imageGroupe,couleurGroupe,dateCreation,description) VALUES (nom, img, clr,  crt_date ,descr) ;
 SET idGrp = LAST_INSERT_ID();
 INSERT INTO Membre(dateAdhesion, status, idInternaute, idRole, idGroupe) VALUES (crt_date,'Présent',id,1,idGrp) ;
+END;
+//
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE OR REPLACE PROCEDURE createInternaute(p_nom VARCHAR(50),p_prenom VARCHAR(50),p_email VARCHAR(50),p_adresse VARCHAR(50),p_hash VARCHAR(100),p_salt VARCHAR(50))
+BEGIN
+INSERT INTO Internaute (nom,prenom,adresse,email,dateCreation,hash,salt) VALUES (p_nom,p_prenom,p_adresse,p_email,CURRENT_DATE,p_hash,p_salt) ;
 END;
 //
 DELIMITER ;
