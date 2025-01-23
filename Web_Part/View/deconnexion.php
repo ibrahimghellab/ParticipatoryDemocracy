4bb36332ffc850fb1e18ef25ee5611c82c3f1f64
Thomas Troles <thomas.troles@universite-paris-saclay.fr>
2025-01-23 11:40:02 +0100

    add page and updating CSS

---
+++ Web_Part/View/deconnexion.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/navbar.css">
+    <link rel="stylesheet" href="../CSS/login.css">
+    <link rel="stylesheet" href="../CSS/default.css">
+
+
+    <title>Log-in</title>
+</head>
+
+<body>
+    <header>
+        <?php
+        include(__DIR__ . "/../View/navbar_connexion.php")
+            ?>
+    </header>
+    <main>
+        <div>
+            <h1>Vous êtes déconnecté</h1>
+            <p>A bientôt !</p>
+        </div>
+       
+    </main>
+
+
+
+</body>
+
+</html>
