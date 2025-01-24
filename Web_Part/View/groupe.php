02c58cfe35bcc3a0fb44155b001771be95c0d35d
Thomas Troles <thomas.troles@universite-paris-saclay.fr>
2025-01-24 12:32:18 +0100

    add groupe page

---
+++ Web_Part/View/groupe.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/groupe.css">
+    
+
+
+    <title>Groupe</title>
+</head>
+
+<body>
+    <header>
+        <?php
+        include(__DIR__ . "/../View/navbar_connecte.php")
+            ?>
+    </header>
+    <main>
+        <div class="groupe">
+            <h1>Vos groupes :</h1>
+        </div>
+        <div>
+
+        </div>
+    </main>
+
+
+
+</body>
+
+</html>
