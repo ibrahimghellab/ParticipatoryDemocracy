e228660978fe988f88a8b83445bb4b6e4624c55b
ibrahimghellab <ghellabibrahim@gmail.com>
2025-01-26 18:13:06 +0100

    Refactor modifierDonnees.php for improved code formatting and consistency

--- Web_Part/View/modifierDonnees.php
+++ Web_Part/View/modifierDonnees.php
@@ -15,37 +15,41 @@
 <body>
     <header>
         <?php
-              session_start(); // Start the session
-              include(__DIR__ . "/../View/navbar_connecte.php");
-              include(__DIR__ . "/../Model/user.php");
-              
+        session_start(); // Start the session
+        include(__DIR__ . "/../View/navbar_connecte.php");
+        include(__DIR__ . "/../Model/user.php");
+
         ?>
     </header>
     <main>
         <div>
-        <h2>Vos informations personnelles :</h2>
+            <h2>Vos informations personnelles :</h2>
 
-            <form method="POST" action="./../Controller/routeur.php" >
+            <form method="POST" action="./../Controller/routeur.php">
                 <input type="hidden" name="controleur" value="UserController">
                 <input type="hidden" name="action" value="updateUser">
                 <div>
-                    <input type="text" name="nom" id="nom" value="<?php echo User::getUserDataFromAPI($_SESSION["id"])["nom"] ?>">
+                    <input type="text" name="nom" id="nom"
+                        value="<?php echo User::getUserDataFromAPI($_SESSION["id"])["nom"] ?>">
                 </div>
 
                 <div>
-                    <input type="text" name="prenom" id="prenom"  value="<?php echo User::getUserDataFromAPI($_SESSION["id"])["prenom"] ?>" >
+                    <input type="text" name="prenom" id="prenom"
+                        value="<?php echo User::getUserDataFromAPI($_SESSION["id"])["prenom"] ?>">
                 </div>
 
                 <div>
-                    <input type="email" id="mail" name="mail"  value="<?php echo User::getUserDataFromAPI($_SESSION["id"])["email"] ?>">
+                    <input type="email" id="email" name="email"
+                        value="<?php echo User::getUserDataFromAPI($_SESSION["id"])["email"] ?>">
                 </div>
 
                 <div>
-                    <input type="text" id="adresse" name="adresse"  value="<?php echo User::getUserDataFromAPI($_SESSION["id"])["adresse"] ?>">
+                    <input type="text" id="adresse" name="adresse"
+                        value="<?php echo User::getUserDataFromAPI($_SESSION["id"])["adresse"] ?>">
                 </div>
 
                 <div>
-                    <input type="password" id="password" name="password"  value="********">
+                    <input type="password" id="password" name="password" value="********">
                 </div>
 
                 <div class="submit">
