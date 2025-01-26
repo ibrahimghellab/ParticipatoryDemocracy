a1166c2f8b303514189366b1d8894ba3335c068a
Thomas220205 <peyronneaud.thomas@gmail.com>
2025-01-26 21:47:35 +0100

    Refactor modifierDonnees.php to improve form structure and add account deletion functionality

--- Web_Part/View/modifierDonnees.php
+++ Web_Part/View/modifierDonnees.php
@@ -8,7 +8,6 @@
     <link rel="stylesheet" href="../CSS/modifierDonnees.css">
     <link rel="stylesheet" href="../CSS/default.css">
 
-
     <title>Log-in</title>
 </head>
 
@@ -18,7 +17,6 @@
         session_start(); // Start the session
         include(__DIR__ . "/../View/navbar_connecte.php");
         include(__DIR__ . "/../Model/user.php");
-
         ?>
     </header>
     <main>
@@ -32,46 +30,35 @@
                     <input type="text" name="nom" id="nom"
                         value="<?php echo User::getUserDataFromAPI($_SESSION["id"])["nom"] ?>">
                 </div>
-
                 <div>
                     <input type="text" name="prenom" id="prenom"
                         value="<?php echo User::getUserDataFromAPI($_SESSION["id"])["prenom"] ?>">
                 </div>
-
                 <div>
                     <input type="email" id="email" name="email"
                         value="<?php echo User::getUserDataFromAPI($_SESSION["id"])["email"] ?>">
                 </div>
-
                 <div>
                     <input type="text" id="adresse" name="adresse"
                         value="<?php echo User::getUserDataFromAPI($_SESSION["id"])["adresse"] ?>">
                 </div>
-
                 <div>
                     <input type="password" id="password" name="password" value="********">
                 </div>
-
                 <div class="submit">
                     <button type="submit" id="submit-button">Sauvegarder</button>
                 </div>
-
             </form>
+
             <form method="POST" action="./../Controller/routeur.php">
                 <input type="hidden" name="controleur" value="UserController">
                 <input type="hidden" name="action" value="deleteUser">
-
                 <div class="submit">
-                    <button type="submit" id="submit-button">Supprimer mon compte</button>
+                    <button type="submit" id="submit-button2">Supprimer mon compte</button>
                 </div>
-
             </form>
         </div>
-
     </main>
-
-
-
 </body>
 
 </html>
