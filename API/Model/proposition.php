2efa4f7edbf848377b6a4a3ac137946bad59647b
osmtug <tugrulosman5@gmail.com>
2025-01-26 15:44:32 +0100

    API for proposition

--- API/Model/proposition.php
+++ API/Model/proposition.php
@@ -4,11 +4,15 @@ class Proposition
     public static function getPropositionById($id)
     {
         require_once(__DIR__ . "/../config/connexion.php");
-        $requeteAvecTags = "";
+        $requeteAvecTags = "SELECT * FROM Proposition WHERE idProposition=:id;";
         $requetePreparee = Connexion::pdo()->prepare($requeteAvecTags);
 
-
-        $requetePreparee->bindParam(":?", $id, PDO::PARAM_INT);
+        $valeurs = array();
+        if (strlen($id) > 0) {
+            $valeurs["id"] = $id;
+        } else {
+            $valeurs["id"] = NULL;
+        }
 
         try {
             $requetePreparee->execute($valeurs);
@@ -19,5 +23,108 @@ class Proposition
             echo $e->getMessage();
         }
     }
+
+    public static function createProposition()
+    {
+        $body = file_get_contents("php://input");
+        $tab = json_decode($body, true);
+        if (isset($tab["titre"]) && isset($tab["description"]) && isset($tab["theme"]) && isset($tab["status"]) && isset($tab["idMembre"])) {
+            require_once(__DIR__ . "/../config/connexion.php");
+
+            $sql = "INSERT INTO Proposition(titre, description, dateCreation, theme, status, idMembre)"
+            $sql += " VALUES (:titre, :description, CURRENT_DATE(), :theme:, :status, :idMembre);"
+
+            $requetePreparee = Connexion::pdo()->prepare();
+            $requetePreparee->bindParam(":titre", $tab["titre"], PDO::PARAM_STR);
+            $requetePreparee->bindParam(":description", $tab["description"], PDO::PARAM_STR);
+            $requetePreparee->bindParam(":theme", $tab["theme"], PDO::PARAM_STR);
+            $requetePreparee->bindParam(":status", $tab["status"], PDO::PARAM_STR);
+            $requetePreparee->bindParam(":idMembre", $tab["idMembre"], PDO::PARAM_INT);
+
+            try {
+                $requetePreparee->execute();
+            } catch (PDOException $e) {
+                header("HTTP/1.1 500 Internal Server Error");
+                return json_encode(array("message" => "false"));
+            }
+        } else {
+            header("HTTP/1.1 500 Internal Server Error");
+            return json_encode(array("message" => "false"));
+        }
+
+        return json_encode(array("message" => "true"));
+    }
+
+    public static function updateProposition($id)
+    {
+        $body = file_get_contents("php://input");
+        $tab = json_decode($body, true);
+        $result = false;
+        if (isset($tab["titre"]) && isset($tab["description"]) && isset($tab["theme"]) && isset($tab["status"]) && isset($tab["idVote"])) {
+            require_once(__DIR__ . "/../config/connexion.php");
+
+            $requetePreparee = Connexion::pdo()->prepare("UPDATE Proposition SET titre=:titre, description=:description,theme=:theme,status=:status,idVote=:idVote WHERE idProposition=:id;");
+            $requetePreparee->bindParam(":titre", $tab["titre"], PDO::PARAM_STR);
+            $requetePreparee->bindParam(":description", $tab["description"], PDO::PARAM_STR);
+            $requetePreparee->bindParam(":theme", $tab["theme"], PDO::PARAM_STR);
+            $requetePreparee->bindParam(":status", $tab["status"], PDO::PARAM_STR);
+            $requetePreparee->bindParam(":idVote", $tab["idVote"], PDO::PARAM_INT);
+            $requetePreparee->bindParam(":id", $id, PDO::PARAM_INT);
+            try {
+                $requetePreparee->execute();
+            } catch (PDOException $e) {
+                header("HTTP/1.1 500 Internal Server Error");
+                return json_encode(array("message" => "false"));
+            }
+        } else {
+            header("HTTP/1.1 500 Internal Server Error");
+            return json_encode(array("message" => "false"));
+        }
+
+        return json_encode(array("message" => "true"));
+    }
+
+    public static function deleteProposition($id)
+    {
+        $body = file_get_contents("php://input");
+        $tab = json_decode($body, true);
+        $result = false;
+
+        require_once(__DIR__ . "/../config/connexion.php");
+
+        $requetePreparee = Connexion::pdo()->prepare("DELETE FROM Proposition WHERE idProposition=:id;");
+        $requetePreparee->bindParam(":id", $id, PDO::PARAM_INT);
+        try {
+            $requetePreparee->execute();
+            $result = true;
+        } catch (PDOException $e) {
+            http_response_code(500);
+            return json_encode($response = [
+                "code" => http_response_code(500),
+                "message" => $e->getMessage()
+            ], JSON_PRETTY_PRINT);
+        }
+
+        if ($result) {
+            http_response_code(200);
+            $response = [
+                "code" => http_response_code(200),
+                "message" => "Proposition  supprimé."
+            ];
+
+        } else {
+            http_response_code(500);
+            $response = [
+                "code" => http_response_code(500),
+                "message" => "ERREUR: La Proposition n'as pas été supprimé."
+            ];
+        }
+
+
+
+        return (json_encode($response, JSON_PRETTY_PRINT));
+
+    }
+
 }
 ?>
--- API/router/routeProposition.php
+++ API/router/routeProposition.php
@@ -6,24 +6,15 @@ Connexion::connect();
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     if (!empty($url[1]) && is_numeric($url[1]) && empty($url[2])) {
         echo json_encode(Proposition::getPropositionById($url[1]), JSON_PRETTY_PRINT);
-    } elseif (!empty($url[1]) && is_numeric($url[1]) && $url[2] == "groupes") {
-        echo json_encode(User::getGroupesByUser($url[1]), JSON_PRETTY_PRINT);
-    } elseif (empty($url[1])) {
-        echo "coucou";
-        echo User::checkUser();
     } else {
         throw new Exception("Test ??");
     }
 } elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if ($url[1] == "get-id") {
-        echo User::checkUser();
-    } else {
-        echo User::createUser();
-    }
+    echo Proposition::createProposition();
 } elseif ($_SERVER["REQUEST_METHOD"] == "PUT") {
-    echo User::updateUser($url[1]);
+    echo Proposition::updateProposition($url[1]);
 } elseif ($_SERVER["REQUEST_METHOD"] == "DELETE") {
-    echo User::deleteUser($url[1]);
+    echo Proposition::deleteProposition($url[1]);
 }
 
 ?>
