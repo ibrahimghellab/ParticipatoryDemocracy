4956aa05a56bf8df7b3da7131a09984f87eb2424
Thomas220205 <peyronneaud.thomas@gmail.com>
2025-01-27 17:26:39 +0100

    Update navbar to link to 'Mes groupes' and rename 'Modifier compte' to 'Mon profil'

--- Web_Part/View/navbar_connecte.php
+++ Web_Part/View/navbar_connecte.php
@@ -12,7 +12,7 @@
       <form method="POST" action="./../Controller/UserController.php">
       <button type="submit">déconnexion</button>
       </form> -->
-      <li><a href="./../Controller/routeur.php?controleur=UserControlleur&action=disconnect">déconnexion</a></li>
+      <li><a href="./../View/groupe.php">Mes groupes</a></li>
 
       <!-- </li> -->
       <li><a href="./signup.php">créer un compte</a></li>
@@ -21,9 +21,9 @@
         <div class="dropdown">
           <button class="btn">Dropdown</button>
           <div class="liste-pages">
-            <a href="./../View/modifierDonnees.php">Modifier compte</a>
-            <a href="">2</a>
-            <a href="">3</a>
+            <a href="./../View/modifierDonnees.php">Mon profil</a>
+            <a href="./../Controller/routeur.php?controleur=UserControlleur&action=disconnect">déconnexion</a>
+
           </div>
         </div>
       </li>
