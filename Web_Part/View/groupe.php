ded81fe58b7ea8afa1d41b1de1fdbd9a152c997b
Thomas220205 <peyronneaud.thomas@gmail.com>
2025-01-27 18:30:01 +0100

    Refactor CSS styles in groupe.css for clarity and consistency; clean up groupe.php by removing unnecessary whitespace

--- Web_Part/View/groupe.php
+++ Web_Part/View/groupe.php
@@ -5,9 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/groupe.css">
-
-
-
     <title>Groupe</title>
 </head>
 
@@ -15,7 +12,7 @@
     <header>
         <?php
         include(__DIR__ . "/../View/navbar_connecte.php")
-            ?>
+        ?>
     </header>
     <main>
         <div class="groupes">
@@ -25,11 +22,9 @@
                     <input type="hidden" name="action" value="createGroupe">
 
                     <div class="submit">
-                        <button type="submit" id="submit-button">+ Nouveau Groupe</button>
+                        <button type="submit" id="new-groupe">+ Nouveau Groupe</button>
                     </div>
-
                 </form>
-
             </div>
 
             <?php
@@ -50,17 +45,8 @@
                 echo '</div>';
             }
             ?>
-
         </div>
-        <div>
-
-        </div>
-
-
     </main>
-
-
-
 </body>
 
 </html>
