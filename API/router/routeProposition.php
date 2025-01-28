8b6555750cf9db13b87cb6c2a0298d382fdeaead
ibrahimghellab <ghellabibrahim@gmail.com>
2025-01-28 01:20:08 +0100

    Add endpoints to retrieve reactions and comments by proposition ID in routeProposition.php

--- API/router/routeProposition.php
+++ API/router/routeProposition.php
@@ -6,6 +6,10 @@ Connexion::connect();
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     if (!empty($url[1]) && is_numeric($url[1]) && empty($url[2])) {
         echo json_encode(Proposition::getPropositionById($url[1]), JSON_PRETTY_PRINT);
+    } elseif (!empty($url[1]) && is_numeric($url[1]) && $url[2] == "reactions") {
+        echo json_encode(Proposition::getReactionByProposition($url[1]), JSON_PRETTY_PRINT);
+    } elseif (!empty($url[1]) && is_numeric($url[1]) && $url[2] == "commentaires") {
+        echo json_encode(Proposition::getCommentaireByProposition($url[1]), JSON_PRETTY_PRINT);
     } else {
         throw new Exception("Test ??");
     }
