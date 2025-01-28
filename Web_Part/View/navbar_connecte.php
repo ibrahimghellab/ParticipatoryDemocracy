218be6b568ac5b76ebc84bd28cb7abc71dcdc758
ibrahimghellab <ghellabibrahim@gmail.com>
2025-01-28 01:20:18 +0100

    Refactor navbar_connecte.php for improved HTML structure and indentation

--- Web_Part/View/navbar_connecte.php
+++ Web_Part/View/navbar_connecte.php
@@ -1,30 +1,31 @@
 <head>
-  <link rel="stylesheet" href="../CSS/navbar_connecte.css">
+    <link rel="stylesheet" href="../CSS/navbar_connecte.css">
 </head>
 
 <nav>
-  <div>
-    <img src="../assets/logol-removebg-preview.png" alt="logo nav bar" />
-  </div>
-  <div class="nav-right">
-    <ul>
-      <!-- <li>
+    <div>
+        <img src="../assets/logol-removebg-preview.png" alt="logo nav bar" />
+    </div>
+    <div class="nav-right">
+        <ul>
+            <!-- <li>
       <form method="POST" action="./../Controller/UserController.php">
       <button type="submit">déconnexion</button>
       </form> -->
-      <li><a href="./../View/groupe.php">Mes groupes</a></li>
+            <li><a href="./../View/groupe.php">Mes groupes</a></li>
 
 
-      <!-- Menu déroulant -->
-      <li class="menu">
-        <div class="dropdown">
-            <button class="btn menu-btn"><img src="./../assets/menu.svg" alt="menu" class="menu-icon"></button>          <div class="liste-pages">
-            <a href="./../View/modifierDonnees.php">Mon profil</a>
-            <a href="./../Controller/routeur.php?controleur=UserControlleur&action=disconnect">déconnexion</a>
+            <!-- Menu déroulant -->
+            <li class="menu">
+                <div class="dropdown">
+                    <button class="btn menu-btn"><img src="./../assets/menu.svg" alt="menu" class="menu-icon"></button>
+                    <div class="liste-pages">
+                        <a href="./../View/modifierDonnees.php">Mon profil</a>
+                        <a href="./../View/login.php">déconnexion</a>
 
-          </div>
-        </div>
-      </li>
-    </ul>
-  </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
 </nav>
