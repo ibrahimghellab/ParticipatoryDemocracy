dfaed7fdc818b7f9793faa5c6bf925fcd268d680
ibrahimghellab <ghellabibrahim@gmail.com>
2025-01-31 19:02:52 +0100

    refactor proposition view to streamline member display and deletion functionality

--- Web_Part/View/proposition.php
+++ Web_Part/View/proposition.php
@@ -47,63 +47,70 @@
                     ?>
                 </tbody>
             </table>
-            
-            <div class="colonne">
-                    <?php require_once(__DIR__ . "/../Controller/GroupeController.php");
-                    echo '<h2>Proposition</h2>';
 
-                    require_once(__DIR__ . "/../Model/groupe.php");
-                    $tab = Groupe::getPropositionsByGroupe();
-                    for ($i = 0; $i < count($tab); $i++) {
-                        echo "<div class='proposition'>";
+            <div class="colonne">
+                <?php require_once(__DIR__ . "/../Controller/GroupeController.php");
+                echo '<h2>Proposition</h2>';
+
+                require_once(__DIR__ . "/../Model/groupe.php");
+                $tab = Groupe::getPropositionsByGroupe();
+                $users = Groupe::getMembresByGroupe();
+                $idMembre = null;
+                for ($i = 0; $i < count($users); $i++) {
+                    if ($users[$i]["idInternaute"] != $_SESSION["id"]) {
+                        $idMembre = $users[$i]["idMembre"];
+                    }
+                }
+                for ($i = 0; $i < count($tab); $i++) {
+                    echo "<div class='proposition'>";
+                    echo '<form method="POST" action="./../Controller/routeur.php" style="flex-grow: 1; display: flex; align-items: center;">';
+                    echo '<input type="hidden" name="idMembre" value="' . $idMembre . '">';
+                    echo '<input type="hidden" name="idGroupe" value="' . $tab[$i]["idGroupe"] . '">';
+                    echo '<input type="hidden" name="idProposition" value="' . $tab[$i]["idProposition"] . '">';
+                    echo '<input type="hidden" name="titre" value="' . $tab[$i]["titre"] . '">';
+                    echo '<input type="hidden" name="description" value="' . $tab[$i]["description"] . '">';
+                    echo '<input type="hidden" name="dateCreation" value="' . $tab[$i]["dateCreation"] . '">';
+                    echo '<input type="hidden" name="theme" value="' . $tab[$i]["theme"] . '">';
+                    echo '<input type="hidden" name="controleur" value="PropositionController">';
+                    echo '<input type="hidden" name="action" value="afficherProposition">';
+                    echo '<button type="submit" class="proposition-button">';
+                    echo '<div style="display:flex;align-items:center;width:100%;pointer-events:none;">' . $tab[$i]["titre"] . '<div style="border-radius:50%;width:20px;height:20px;background-color:' . $tab[$i]["theme"] . ';margin-left:10px;"></div></div>';
+                    echo '</button>';
+                    echo '<span class="date-creation" style="margin-left: 600px;">' . $tab[$i]["dateCreation"] . '</span>';
+                    echo '</form>';
+                    echo '</div>';
+                }
+                ?>
+
+
+                <div>
+                    <?php
+                    echo '<h2>Membres du groupe</h2>';
+                    require_once(__DIR__ . "/../Model/user.php");
+                    $users = Groupe::getMembresByGroupe();
+                    for ($i = 0; $i < count($users); $i++) {
+                        echo "<div class='user'>";
                         echo '<form method="POST" action="./../Controller/routeur.php" style="flex-grow: 1; display: flex; align-items: center;">';
-                        echo '<input type="hidden" name="idMembre" value="' . $tab[$i]["idMembre"] . '">';
-                        echo '<input type="hidden" name="idGroupe" value="' . $tab[$i]["idGroupe"] . '">';
-                        echo '<input type="hidden" name="idProposition" value="' . $tab[$i]["idProposition"] . '">';
-                        echo '<input type="hidden" name="titre" value="' . $tab[$i]["titre"] . '">';
-                        echo '<input type="hidden" name="description" value="' . $tab[$i]["description"] . '">';
-                        echo '<input type="hidden" name="dateCreation" value="' . $tab[$i]["dateCreation"] . '">';
-                        echo '<input type="hidden" name="theme" value="' . $tab[$i]["theme"] . '">';
-                        echo '<input type="hidden" name="controleur" value="PropositionController">';
-                        echo '<input type="hidden" name="action" value="afficherProposition">';
-                        echo '<button type="submit" class="proposition-button">';
-                        echo '<div style="display:flex;align-items:center;width:100%;pointer-events:none;">' . $tab[$i]["titre"] . '<div style="border-radius:50%;width:20px;height:20px;background-color:' . $tab[$i]["theme"] . ';margin-left:10px;"></div></div>';
-                        echo '</button>';
-                        echo '<span class="date-creation" style="margin-left: 600px;">' . $tab[$i]["dateCreation"] . '</span>';
+                        echo '<input type="hidden" name="controleur" value="UserController">';
+                        echo '<input type="hidden" name="action" value="deleteMembre">';
+                        echo $users[$i]["nom"] . " " . $users[$i]["prenom"];
+                        if ($users[$i]["idInternaute"] != $_SESSION["id"]) {
+                            echo '<button type="submit" class="delete-user">';
+                            echo 'delete';
+                            echo '</button>';
+                        } else {
+                            echo '<button type="submit" class="delete-user">';
+                            echo 'Quitter le groupe';
+                            echo '</button>';
+                        }
                         echo '</form>';
                         echo '</div>';
                     }
                     ?>
 
-
-                    <div>
-                        <?php
-                         echo '<h2>Membres du groupe</h2>';
-                        require_once(__DIR__ . "/../Model/user.php");
-                        $users = Groupe::getMembresByGroupe();
-                        for ($i = 0; $i < count($users); $i++) {
-                            echo "<div class='user'>";
-                            echo '<form method="POST" action="./../Controller/routeur.php" style="flex-grow: 1; display: flex; align-items: center;">';
-                            echo '<input type="hidden" name="controleur" value="UserController">';
-                            echo '<input type="hidden" name="action" value="deleteMembre">';
-                            echo $users[$i]["nom"] . " " . $users[$i]["prenom"];
-                            if ($users[$i]["idInternaute"] != $_SESSION["id"]) {
-                                echo '<button type="submit" class="delete-user">';
-                                echo 'delete';
-                                echo '</button>';
-                            } else {
-                                echo '<button type="submit" class="delete-user">';
-                                echo 'Quitter le groupe';
-                                echo '</button>';
-                            }
-                            echo '</form>';
-                            echo '</div>';
-                        }
-                        ?>
-
-                    </div>
-                    </div>
+                </div>
             </div>
+        </div>
     </main>
 </body>
 
