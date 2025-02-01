3cbcad23aa512834f02e78b05cf7e12a80627079
Thomas220205 <peyronneaud.thomas@gmail.com>
2025-02-01 15:06:55 +0100

    update createGroupe.php to set page title and enable file upload in form

--- Web_Part/View/createGroupe.php
+++ Web_Part/View/createGroupe.php
@@ -1,35 +1,32 @@
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/navbar.css">
     <link rel="stylesheet" href="../CSS/createGroupe.css">
     <link rel="stylesheet" href="../CSS/default.css">
-
-
-    <title>Log-in</title>
+    <title>Créer un groupe</title>
 </head>
-
 <body>
+
     <header>
-        <?php
-        include(__DIR__ . "/../View/navbar_connecte.php")
-            ?>
+        <?php include(__DIR__ . "/../View/navbar_connecte.php"); ?>
     </header>
+
     <main>
         <div>
-            <form method="POST" action="./../Controller/routeur.php">
+            <form method="POST" action="./../Controller/routeur.php" enctype="multipart/form-data">
                 <input type="hidden" name="controleur" value="GroupeController">
                 <input type="hidden" name="action" value="createGroupe">
+
                 <div>
-                    <label for="nom">Nom du groupe : </label>
+                    <label for="nom">Nom du groupe :</label>
                     <input type="text" name="nom" id="nom" required>
                 </div>
 
                 <div>
-                    <label for="description">Description : </label>
+                    <label for="description">Description :</label>
                     <input type="text" name="description" id="description" required>
                 </div>
 
@@ -46,14 +43,9 @@
                 <div class="submit">
                     <button type="submit" id="submit-button">Envoyer</button>
                 </div>
-
             </form>
         </div>
-
     </main>
 
-
-
 </body>
-
 </html>
