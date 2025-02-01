3997e1857834d6c0af2bf525e5c18df3a7e9ebe7
ibrahimghellab <ghellabibrahim@gmail.com>
2025-02-01 17:33:44 +0100

    remove unused trigger for email validation in Internaute table

--- DB_Part/triggers.sql
+++ DB_Part/triggers.sql
@@ -26,25 +26,3 @@ END;
 
 
 
-DELIMITER //
-CREATE TRIGGER trg_before_insert_internaute
-BEFORE INSERT ON Internaute
-FOR EACH ROW
-
-v_count NUMBER;
-BEGIN
-SELECT COUNT(*) INTO v_count
-FROM internaute
-WHERE email= :NEW.email;
-IF v_count > 0 THEN
-RAISE_APPLICATION_ERROR(-20001, 'Cette adresse email est déjà utilisée.');
---check if a email is valid with this regex :^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$
-
-
-
-END IF;
-
-END;//
-DELIMITER ;//
-DELIMITER ;
-/
