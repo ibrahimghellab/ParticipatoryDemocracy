0f4d1448d82f60c5f906a8eaf017f0802ba21d2f
ibrahimghellab <ghellabibrahim@gmail.com>
2025-02-01 01:20:17 +0100

    add VoteController to the router for voting functionality

--- Web_Part/Controller/routeur.php
+++ Web_Part/Controller/routeur.php
@@ -7,7 +7,8 @@ $tableauControleurs = [
     "UserController" => __DIR__ . "/../Controller/UserController.php",
     "GroupeController" => __DIR__ . "/../Controller/GroupeController.php",
     "PropositionController" => __DIR__ . "/../Controller/PropositionController.php",
-    "CommentaireController" => __DIR__ . "/../Controller/CommentaireController.php"
+    "CommentaireController" => __DIR__ . "/../Controller/CommentaireController.php",
+    "VoteController" => __DIR__ . "/../Controller/VoteController.php"
 ];
 // Définir le contrôleur et l'action par défaut
 $controleur = "UserController";
