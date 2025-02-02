a2633badaaf341161b6b4f6cb3d4c988c8c25565
ibrahimghellab <ghellabibrahim@gmail.com>
2025-02-02 20:07:36 +0100

    add method to retrieve voting choices and improve error handling in vote processing

--- API/Model/vote.php
+++ API/Model/vote.php
@@ -5,6 +5,7 @@ class Vote
     {
         $body = file_get_contents("php://input");
         $tab = json_decode($body, true);
+        print_r($tab);
         $idVote = null;
         if (isset($tab["idProposition"]) && isset($tab["typeScrutin"]) && isset($tab["dateFin"]) && isset($tab["choixVote"])) {
             require_once(__DIR__ . "/../config/connexion.php");
@@ -18,10 +19,11 @@ class Vote
                 $requetePreparee->execute();
                 $idVote = $requetePreparee->fetchColumn();
             } catch (PDOException $e) {
+                echo $e->getMessage();
                 header("HTTP/1.1 500 Internal Server Error");
                 return json_encode(array("message" => "false"));
             }
-            foreach ($tab["choixVote"] as $choix) {
+            foreach (json_decode($tab["choixVote"], true) as $choix) {
                 $requetePreparee = Connexion::pdo()->prepare("INSERT INTO ChoixVote(possibiliteChoixVote,idVote) VALUES (:possibiliteChoixVote,:idVote)");
                 $requetePreparee->bindParam(":possibiliteChoixVote", $choix, PDO::PARAM_STR);
                 $requetePreparee->bindParam(":idVote", $idVote, PDO::PARAM_INT);
@@ -39,7 +41,22 @@ class Vote
             return json_encode(array("message" => "false"));
         }
 
-        return json_encode(array("message" => "true"));
+        return json_encode(array("message" => "true", "idVote" => $idVote));
+    }
+
+    public static function getChoixVote($id)
+    {
+        require_once(__DIR__ . "/../config/connexion.php");
+        $requetePreparee = Connexion::pdo()->prepare("SELECT * FROM ChoixVote WHERE idVote=:idVote");
+        $requetePreparee->bindParam(":idVote", $id, PDO::PARAM_INT);
+        try {
+            $requetePreparee->execute();
+            $result = $requetePreparee->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            header("HTTP/1.1 500 Internal Server Error");
+            return json_encode(array("message" => "false"));
+        }
+        return $result;
     }
 }
 
