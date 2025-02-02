8167566e28821fe8fad92c54973fdb1e7463bf9e
ibrahimghellab <ghellabibrahim@gmail.com>
2025-02-02 22:11:24 +0100

    add: implement getThemeById method to retrieve theme by ID

--- API/Model/theme.php
+++ API/Model/theme.php
@@ -1,6 +1,22 @@
 <?php
 class Theme
 {
+    public static function getThemeById($id)
+    {
+        require_once(__DIR__ . "/../config/connexion.php");
 
+        $requeteAvecTags = "SELECT * FROM Theme WHERE idTheme= :id ;";
+        $requetePreparee = Connexion::pdo()->prepare($requeteAvecTags);
+        $requetePreparee->bindParam(":id", $id, PDO::PARAM_INT);
+        try {
+            $requetePreparee->execute();
+
+            $requetePreparee->setFetchmode(PDO::FETCH_ASSOC);
+            return $requetePreparee->fetch();
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
 }
 ?>
