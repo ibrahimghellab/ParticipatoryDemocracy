f319bbc95a6f41301d6c004848a1daf882a42a71
ibrahimghellab <ghellabibrahim@gmail.com>
2025-02-02 20:07:42 +0100

    add conditional check to retrieve voting choices based on proposition ID

--- API/router/routeVote.php
+++ API/router/routeVote.php
@@ -4,7 +4,9 @@ require_once(__DIR__ . "/../Model/vote.php");
 
 Connexion::connect();
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-
+    if (!empty($url[1]) && is_numeric($url[1]) && $url[2] == "choix") {
+        echo json_encode(Vote::getChoixVote($url[1]));
+    }
 } elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo Vote::createVote();
 } elseif ($_SERVER["REQUEST_METHOD"] == "PUT") {
