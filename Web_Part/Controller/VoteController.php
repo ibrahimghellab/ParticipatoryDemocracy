ac373fa5d44fedd7c0b919387b65b162e0cc8764
ibrahimghellab <ghellabibrahim@gmail.com>
2025-02-02 20:07:53 +0100

    refactor createVote method to return voting data

--- Web_Part/Controller/VoteController.php
+++ Web_Part/Controller/VoteController.php
@@ -4,13 +4,15 @@ class VoteController
 {
     public static function createVote()
     {
-        Vote::createVote();
+        $tab = Vote::createVote();
         require_once(__DIR__ . "/../View/inside_proposition.php");
+        return $tab;
     }
 
     public static function afficherFormulaire()
     {
         require_once(__DIR__ . "/../View/createVote.php");
     }
+
 }
 ?>
