699980671b3e1becb4a183fd01494677455c32f9
ibrahimghellab <ghellabibrahim@gmail.com>
2025-02-02 17:42:44 +0100

    refactor createVote method to be static and update JSON data structure for POST request

--- Web_Part/Model/vote.php
+++ Web_Part/Model/vote.php
@@ -17,7 +17,7 @@ class Vote extends Modele
         }
     }
 
-    public function createVote()
+    public static function createVote()
     {
         $url = 'https://projets.iut-orsay.fr/saes3-ttroles/API/vote/'; // Exemple d'URL d'API
 
@@ -25,7 +25,15 @@ class Vote extends Modele
         $ch = curl_init($url);
 
         // Encoder les données en JSON
-        $jsonData = json_encode($_POST);
+        $jsonData = [
+            'idProposition' => $_POST['idProposition'],
+            'typeScrutin' => $_POST['typeScrutin'],
+            'dateFin' => $_POST['dateFin'],
+            'choixVote' => $_POST['choixVote']
+        ];
+
+        $jsonData = json_encode($jsonData);
+
 
         // Définir les options cURL pour une requête POST
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  // Retourner la réponse sous forme de chaîne
