5c01a8d824cbbee3d1e66b87a8b7767e766aa17e
ibrahimghellab <ghellabibrahim@gmail.com>
2025-02-03 14:57:18 +0100

    Refactor MembreController to update createMembre() and change invitation sending method

--- Web_Part/Model/membre.php
+++ Web_Part/Model/membre.php
@@ -24,7 +24,7 @@ class Membre extends Modele
         $objet = "Invitation à rejoindre notre groupe";
         $corps = "Bonjour,%0A%0AVous%20avez%20reçu%20une%20invitation%20à%20rejoindre%20notre%20groupe.%20Cliquez%20sur%20le%20lien%20pour%20accepter%20ou%20refuser:%20" . urlencode("https://projets.iut-orsay.fr/saes3-ttroles/Web_Part/View/signupToken.php?token=" . $token.'&idGroupe='.$_POST["id"].'&role='.$_POST["role"]);
         $mailto_link = "mailto:" . $destinataire . "?subject=" . urlencode($objet) . "&body=" . $corps;
-        echo "Cliquez pour envoyer l'invitation : <a href='" . $mailto_link . "'>Envoyer l'invitation</a>";        
+        header("Location: " . $mailto_link);  
     }
 
     public static function createMembre(){
