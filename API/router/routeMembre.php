e35e75fc7405746217ccccfee5612bc74544e6a1
Thomas Troles <thomas.troles@universite-paris-saclay.fr>
2025-02-05 14:08:23 +0100

    route update

--- API/router/routeMembre.php
+++ API/router/routeMembre.php
@@ -4,7 +4,9 @@ require_once(__DIR__ . "/../Model/membre.php");
 
 Connexion::connect();
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-
+    if(!empty($url[1]) && is_numeric($url[1])){
+        Membre::getMembreById($url[1]);
+    }
 } elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!empty($url[1]) && is_numeric($url[1]) && !empty($url[2]) && $url[2] == "vote" && !empty($url[3]) && is_numeric($url[3])) {
         echo Membre::vote($url[1], $url[3]);
