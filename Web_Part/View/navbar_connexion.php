32f137f5b515611c74fa23d1adf30e11e5f55d8c
ibrahimghellab <ghellabibrahim@gmail.com>
2025-02-06 02:04:18 +0100

    fix: update logo image source and adjust styling in navbar_connexion.php

--- Web_Part/View/navbar_connexion.php
+++ Web_Part/View/navbar_connexion.php
@@ -1,10 +1,11 @@
 <nav>
   <div>
-    <img src="../assets/logol-removebg-preview.png" alt="logo nav bar" />
+    <img src="../assets/logo.png" alt="logo nav bar" style="width: 50px;
+  height: 50px;margin : 10px" />
   </div>
   <div class="nav-right">
     <ul>
-     
+
       <li><a href="./login.php">se connecter</a></li>
       <li><a href="./signup.php">créer un compte</a></li>
 
